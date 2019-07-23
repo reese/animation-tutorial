e42764784bdf073b03c8c16116a1040798eebcc2
Reese Williams <rtwill722@gmail.com>
2019-07-23 13:13:40 -0400

    Change App/index to App/App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,78 +1,16 @@
 import React, { Component } from 'react'
-import { Animated, Text, View } from 'react-native'
-import {
-  PanGestureHandler,
-  PanGestureHandlerGestureEvent,
-  PanGestureHandlerStateChangeEvent,
-  State as GestureState
-} from 'react-native-gesture-handler'
+import { Text, View } from 'react-native'
 
 import { Wheel } from 'src/Wheel/Wheel'
 import { styles } from './styles'
 
-interface State {
-  rotationAngle: Animated.Value
-  velocity: Animated.Value
-}
-
-const ROTATION_THROTTLE = 20
-const VELOCITY_THROTTLE = 2000
-
-export class App extends Component<State> {
-  public state = {
-    rotationAngle: new Animated.Value(0),
-    velocity: new Animated.Value(0)
-  }
-
+export class App extends Component {
   public render() {
-    const angle = this.calculatedRotationAngle
-
-    const interpolatedAngle = angle.interpolate({
-      inputRange: [-360, 360],
-      outputRange: ["-360deg", "360deg"]
-    })
-
     return (
-      <PanGestureHandler
-        onGestureEvent={this.scrollHandler}
-        onHandlerStateChange={this.scrollEndHandler}
-      >
-        <View style={styles.container}>
-          <Text style={styles.pointer}>👇</Text>
-          <Wheel rotationAngle={interpolatedAngle} />
-        </View>
-      </PanGestureHandler>
+      <View style={styles.container}>
+        <Text style={styles.pointer}>👇</Text>
+        <Wheel />
+      </View>
     )
-  }
-
-  private get calculatedRotationAngle() {
-    const { rotationAngle, velocity } = this.state
-
-    const newRotationAngle = Animated.add(rotationAngle, velocity)
-    // @ts-ignore
-    if (velocity._value !== 0) {
-      this.setState({ rotationAngle: newRotationAngle })
-    }
-    return newRotationAngle
-  }
-
-  private scrollHandler = (event: PanGestureHandlerGestureEvent) => {
-    const { rotationAngle } = this.state
-    const offset = event.nativeEvent.translationX / ROTATION_THROTTLE
-    this.setState({ rotationAngle: Animated.add(rotationAngle, new Animated.Value(offset)) })
-  }
-
-  private scrollEndHandler = (event: PanGestureHandlerStateChangeEvent) => {
-    if (event.nativeEvent.state === GestureState.END) {
-      this.decaySpinVelocity(event)
-    }
-  }
-
-  private decaySpinVelocity = (event: PanGestureHandlerGestureEvent) => {
-    const startingVelocity = event.nativeEvent.velocityX / VELOCITY_THROTTLE
-    Animated.decay(this.state.velocity, {
-      useNativeDriver: true,
-      velocity: startingVelocity,
-    }).start()
   }
 }
--- src/Wheel/Wheel.tsx
+++ src/Wheel/Wheel.tsx
@@ -4,19 +4,11 @@ import { Animated } from 'react-native'
 import wheelOfFortune from 'assets/images/wheelOfFortune.png'
 import { styles } from './styles'
 
-interface Props {
-  rotationAngle: Animated.AnimatedInterpolation
-}
-
-export class Wheel extends Component<Props> {
+export class Wheel extends Component {
   public render() {
-    const { rotationAngle } = this.props
     return (
       <Animated.Image
-        style={{
-          ...styles.wheel,
-          transform: [{ rotate: rotationAngle }]
-        }}
+        style={styles.wheel}
         source={wheelOfFortune}
       />
     )
